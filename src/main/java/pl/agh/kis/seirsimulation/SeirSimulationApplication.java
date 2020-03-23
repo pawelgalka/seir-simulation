68ceb55ba9b9fc5ee92303a36ff72eb9ab5fdae9
Paweł <p.k.galka@gmail.com>
2020-03-23 16:33:32 +0100

    Initial commit

---
+++ src/main/java/pl/agh/kis/seirsimulation/SeirSimulationApplication.java
@@ -0,0 +1,13 @@
+package pl.agh.kis.seirsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeirSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeirSimulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/agh/kis/seirsimulation/SeirSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.kis.seirsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeirSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
